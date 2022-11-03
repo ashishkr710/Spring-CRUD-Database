62335e99ffa0ea83342f5a5c48c610680b39c2ca
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2022-11-03 23:08:00 +0530

    Initial commit

---
+++ src/test/java/com/Ashish/Ashish/AshishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ashish.Ashish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
